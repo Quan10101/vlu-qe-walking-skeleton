655b4c58d6c210d49f38e590e679b15d574d1e4a
Quan10101 <hoangquan2492006@gmail.com>
2026-09-16 13:27:55 +0700

    Implement login function with admin credentials check

---
+++ login.js
@@ -0,0 +1,11 @@
+function login(user, pass) {
+    if (user === "admin" && pass === "123") {
+        return true;
+    }
+
+    return false;
+}
+
+if (typeof module !== "undefined") {
+    module.exports = login;
+}
